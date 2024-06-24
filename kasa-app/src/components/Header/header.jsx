e114c380c0ef25a0ccee4a241d9572cd4294891a
Candeve <grandjeanemery@gmail.com>
2024-06-24 10:03:35 +0200

    avancement SCSS

--- kasa-app/src/components/Header/header.jsx
+++ kasa-app/src/components/Header/header.jsx
@@ -2,27 +2,33 @@
 import './header.scss';
 import logo from '../../assets/logo.png';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
-  return (
-    
-    <header className='header-kasa'>
-        <div className='headnavcontainer'>
-      <img src={logo} alt="Logo de Kasa" className="logo-kasa" />
-      <nav className='navbar'>
-        <ul>
-          
-            <Link to="/" className='nav-link'>Accueil
-            </Link>
-            <Link to="/Apropos" className='nav-link'>A propos
-            </Link>
+  const location = useLocation();
 
-        </ul>
-      </nav>
+  return (
+    <header className='header-kasa'>
+      <div className='headnavcontainer'>
+        <img src={logo} alt="Logo de Kasa" className="logo-kasa" />
+        <nav className='navbar'>
+          <ul>
+            <Link
+              to="/"
+              className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+            >
+              Accueil
+            </Link>
+            <Link
+              to="/Apropos"
+              className={`nav-link ${location.pathname === '/Apropos' ? 'active' : ''}`}
+            >
+              A propos
+            </Link>
+          </ul>
+        </nav>
       </div>
     </header>
-    
   );
 }
 
